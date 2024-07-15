f2d45cf31022e78848bf46f3cd3b3c96ed8e61ff
praveenreddy82472 <tumatipraveenreddy23@gmail.com>
2024-07-15 17:54:25 -0500

    echo bot code added

--- research/tele_echo_bot.py
+++ research/tele_echo_bot.py
@@ -1,18 +1,18 @@
 """
-This is a echo bot.
+This is an echo bot.
 It echoes any incoming text messages.
 """
 
 import logging
-
-from aiogram import Bot, Dispatcher, executor, types
+from aiogram import Bot, Dispatcher, types
+from aiogram.utils import executor
 from dotenv import load_dotenv
 import os
 
+# Load environment variables from .env file
 load_dotenv()
 
-
-
+# Get API token from environment variables
 API_TOKEN = os.getenv("TOKEN")
 
 # Configure logging
@@ -22,23 +22,23 @@ logging.basicConfig(level=logging.INFO)
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
 
-
+# Handler for /start and /help commands
 @dp.message_handler(commands=['start', 'help'])
 async def send_welcome(message: types.Message):
     """
-    This handler will be called when user sends `/start` or `/help` command
+    This handler will be called when the user sends `/start` or `/help` command.
     """
     await message.reply("Hi!\nI'm EchoBot!\nPowered by aiogram.")
 
-
-
+# Echo handler for all messages
 @dp.message_handler()
 async def echo(message: types.Message):
-    # old style:
-    # await bot.send_message(message.chat.id, message.text)
-
+    """
+    This handler will be called for any message that is not a command.
+    It echoes the message back to the sender.
+    """
     await message.answer(message.text)
 
-
+# Start polling
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
